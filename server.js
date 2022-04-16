36fc949340b091db7bf46d7c0b71befbebccbc32
Ben <ben.a.burgos@gmail.com>
2022-04-15 21:26:06 -0700

    adds basic new/create route

--- server.js
+++ server.js
@@ -20,39 +20,41 @@ app.get('/pokemon', (req, res) => {
 
 // New Route
 app.get('/pokemon/new', (req, res) => {
-    res.send('You are at the new pokemon page!')
-    console.log('New Route Success')
+    res.render('new.ejs', { 
+        totalNumber: Pokemon.length,
+        weaknessList: Pokemon[0].damages
+    })
 });
 
 // Delete Route
 app.delete('/pokemon/:id', (req, res) => {
     res.send(`You deleted Pokemon # ${req.params.id}`)
     console.log(`Delete request for Pokemon # ${req.params.id} received!`)
-})
+});
 
 // Update Route
 app.put('/pokemon/:id', (req, res) => {
     res.send(`You sent an update request for Pokemon # ${req.params.id}`)
     console.log(`Update request for Pokemon # ${req.params.id} received!`)
-})
+});
 
 // Create Route
 app.post('/pokemon', (req, res) => {
     res.send(`You created a new pokemon!`)
     console.log('New pokemon request received')
-})
+});
 
 // Edit Route
 app.get('/pokemon/:id/edit', (req, res) => {
     res.send(`You've reached the page to edit Pokemon # ${req.params.id}`)
     console.log('Edit Route Success')
-})
+});
 
 // Show Route
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', { singlePokemon: Pokemon[req.params.id] })
-})
+});
 
 // Listener
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`You're listening live on port ${PORT}`))
+app.listen(PORT, () => console.log(`You're listening live on port ${PORT}`));
